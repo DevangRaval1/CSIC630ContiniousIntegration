3fe8833ab0ef1b6260b4050baa15cfea05549bec
DevangRaval1 <draval@mail.csuchico.edu>
2023-02-16 10:20:41 -0800

    Added changes for linter trigger

--- hello.cpp
+++ hello.cpp
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
-int main() 
-{
+int main() {
     int a = 10;
     int b = 20;
     cout << "Sum = " << a+b <<endl;
     cout << "Product" << a*b <<endl;
     cout << "Hello World" << endl;
+    if (a == 10 ){
+        cout << "Its 10" << endl;
+    }
     return 0;
 }
-=======
-#include <iostream>
-using namespace std;
-
-int main() 
-{
-    int a = 10;
-    int b = 20;
-    cout << "Sum = " << a+b <<endl;
-    cout << "Product" << a*b <<endl;
-    cout << "Hello World" << endl;
-    return 0;
-}
->>>>>>> 29c5b58f664ce9f02099da78e12a4d4a8337048f
