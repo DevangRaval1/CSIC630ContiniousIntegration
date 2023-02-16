d8dce75f43606858ce55d238f3e5f355f14c5095
DevangRaval1 <draval@mail.csuchico.edu>
2023-02-16 10:24:46 -0800

    Solved some linter errors

--- hello.cpp
+++ hello.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     int a = 10;
     int b = 20;
     cout << "Sum = " << a+b <<endl;
     cout << "Product" << a*b <<endl;
     cout << "Hello World" << endl;
-    if (a == 10 ){
+    if (a == 10)
+    {
         cout << "Its 10" << endl;
     }
     return 0;
