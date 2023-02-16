635cb9db68e5ebed19b50ce758d4f44698c6a6af
DevangRaval1 <draval@mail.csuchico.edu>
2023-02-16 10:13:49 -0800

    Some changes to main

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a = 10;
+    int b = 20;
+    cout << "Sum = " << a+b <<endl;
+    cout << "Product" << a*b <<endl;
+    cout << "Hello World" << endl;
+    return 0;
+}
